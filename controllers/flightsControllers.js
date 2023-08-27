38275a446ed3f315f532ffb3c28829e0b5cf47bf
Kateattardo <youremail@domain.com>
2023-08-27 12:42:27 -0400

    controllers/flightsControllers.js

--- controllers/flightsControllers.js
+++ controllers/flightsControllers.js
@@ -14,65 +14,61 @@ module.exports = {
 
 //index functionality
 function index(req, res) {
-    console.log('these are the flights')
     Flight.find({})
-    .then(flightDocs => {
-        console.log('flight info')
-        res.render('flights/index', {flights: flightDocs, title: "Express"})
-    })
-   .catch(err => {
-    console.log ('===err===')
-    console.log(err)
-    console.log('===err===')
-    return res.send('err creating - check terminal')
- })
+        .then(flightDocs => {
+            console.log('flight info')
+            res.render('flights/index', { flights: flightDocs, title: "Express" })
+        })
+        .catch(err => {
+            console.log('===err===')
+            // console.log(err)
+            log('===err===')
+            return res.send('err creating - check terminal')
+        })
 }
 
 //new 
 function newFlight(req, res) {
     console.log('the new route had been hit')
-    res.render('flights/new', {title: "New Flight"})
+    res.render('flights/new', { title: "New Flight" })
 
 }
 //create
-function create(req, res)  {
+function create(req, res) {
     const newFlight = new Flight(req.body);
-     newFlight.save()
-    .then(flight => {
-        console.log('Flight saved:', flight);
-        res.redirect('/');  // redir to flights list
-    })
-    .catch(error => {
-        console.error('Error saving flight:', error);
-        res.status(500).send('Internal Server Error');
-    });
+    newFlight.save()
+        .then(flight => {
+            console.log('Flight saved:', flight);
+            res.redirect('/');  // redir to flights list
+        })
+        .catch(error => {
+            console.error('Error saving flight:', error.message);
+            res.status(500).send('Internal Server Error');
+        });
 };
 
 //Show
 async function show(req, res) {
- let flight = await   Flight.findById(req.params.id)
-//  .then(function(err, flights) {
-        // Ticket.find({flight: flights._id}).then(function(err, tickets) {
-        //     res.render('flights/show', { tickets});
-        // });
-        res.render('flights/show', { flight});
-    // });
+    console.log('show')
+    let flight = await Flight.findById(req.params.id)
+    const tickets = await Ticket.find({flight: flight._id})
+    res.render('flights/show', { flight, tickets });
 }
-       
- 
+
+
 //Destinations
- function addDestination(req, res) {
+function addDestination(req, res) {
     Flight.findById(req.params.id)
         .then(flight => {
-          flight.destinations.push(req.body); 
-          return flight.save();
+            flight.destinations.push(req.body);
+            return flight.save();
         })
         .then(() => {
-           res.redirect(`/flights/${req.params.id}`);
+            res.redirect(`/flights/${req.params.id}`);
         })
         .catch(err => {
-            console.log(err);
+            console.log('Add destination error:', err.message);
             res.redirect(`/flights/${req.params.id}`);
         });
- }
+}
 
