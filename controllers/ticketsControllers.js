9ea738d3876f2b451b5c08aa7f2207324382f2bc
Kateattardo <youremail@domain.com>
2023-08-27 12:43:06 -0400

    controllers/ticketsControllers.js

--- controllers/ticketsControllers.js
+++ controllers/ticketsControllers.js
@@ -2,20 +2,27 @@ const Ticket = require('../models/tickets');
 const Flight = require('../models/flight')
 
 function newTicket(req, res) {
-    const flightId = req.params.id;
-    res.render('tickets/new', { flightId: req.params.id });
-  }
+  console.log('req.params', req.params)
+  res.render('tickets/new', { flightId: req.params.flightId });
+}
 
-  function create(req, res) {
-
-    Ticket.create(req.body)
-      .then((tickets) => {
-        res.redirect(`/flights/${req.body.flight}`);
-      })
-      .catch((err) => {
-        console.error("Error creating ticket:", err);
-        res.redirect(`/flights/${req.body.flight}/tickets/new`);
-      });
+function create(req, res) {
+  console.log('create', req.body)
+  const ticketData = {
+    seat: req.body.seat,
+    price: req.body.price,
+    flight: req.params.flightId
   }
-  module.exports = {newTicket,
-create}
+  Ticket.create(ticketData)
+    .then((tickets) => {
+      res.redirect(`/flights/${req.params.flightId}`);
+    })
+    .catch((err) => {
+      console.error("Error creating ticket:", err.message);
+      res.redirect(`/flights/${req.body.flight}/tickets/new`);
+    });
+}
+module.exports = {
+  newTicket,
+  create
+}
